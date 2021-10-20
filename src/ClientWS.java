4a0c1a1762cb58921c800f50663da9194eef3fdd
Othman Nouaoury <noouaoury_othman@emsi-edu.ma>
2021-10-20 21:44:40 +0100

    projet

---
+++ src/ClientWS.java
@@ -0,0 +1,20 @@
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stubWS = new BanqueWS().getBanqueServicePort();
+        System.out.println(stubWS.conversionEuroToDH(800));
+        Compte cp = stubWS.getCompte(2L);
+        System.out.println(cp.getCode());
+        List<Compte> cpts = stubWS.compteList();
+        cpts.forEach(c->{
+            System.out.println("-------------------");
+            System.out.println(c.getCode());
+            System.out.println(c.getMontant());
+        });
+    }
+}
